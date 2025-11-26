a1d92f6cc46d6ed5c37b2f2c447e924c13d98b16
kako-jun <3541096+kako-jun@users.noreply.github.com>
2025-11-26 18:55:58 +0900

    Format badge icons

--- src/components/badges/icons/MusicIcon.svelte
+++ src/components/badges/icons/MusicIcon.svelte
@@ -4,14 +4,7 @@
 </script>
 
 <BaseIcon {color}>
-  <ellipse
-    cx="11.1"
-    cy="17.2"
-    rx="3.2"
-    ry="2.2"
-    transform="rotate(-15 11.1 17.2)"
-    fill={color}
-  />
+  <ellipse cx="11.1" cy="17.2" rx="3.2" ry="2.2" transform="rotate(-15 11.1 17.2)" fill={color} />
   <rect x="13" y="5" width="1.8" height="12" rx=".9" fill={color} />
   <path
     d="M15 5c3.1 1.1 5.1 3.6 5.1 6.6 0 1.3-.4 2.7-1 3.7-.3.6-.9.8-1.4.5-.5-.3-.6-1-.3-1.5.5-.9.7-2 .7-3 0-1.7-.8-3.3-2.4-4.4V5Z"
--- src/components/badges/icons/SunIcon.svelte
+++ src/components/badges/icons/SunIcon.svelte
@@ -9,40 +9,8 @@
   <rect x="11" y="19.5" width="2" height="3" rx="1" fill={color} />
   <rect x="1.5" y="11" width="3" height="2" rx="1" fill={color} />
   <rect x="19.5" y="11" width="3" height="2" rx="1" fill={color} />
-  <rect
-    x="11"
-    y="1.5"
-    width="2"
-    height="3"
-    rx="1"
-    fill={color}
-    transform="rotate(45 12 12)"
-  />
-  <rect
-    x="11"
-    y="1.5"
-    width="2"
-    height="3"
-    rx="1"
-    fill={color}
-    transform="rotate(-45 12 12)"
-  />
-  <rect
-    x="11"
-    y="19.5"
-    width="2"
-    height="3"
-    rx="1"
-    fill={color}
-    transform="rotate(45 12 12)"
-  />
-  <rect
-    x="11"
-    y="19.5"
-    width="2"
-    height="3"
-    rx="1"
-    fill={color}
-    transform="rotate(-45 12 12)"
-  />
+  <rect x="11" y="1.5" width="2" height="3" rx="1" fill={color} transform="rotate(45 12 12)" />
+  <rect x="11" y="1.5" width="2" height="3" rx="1" fill={color} transform="rotate(-45 12 12)" />
+  <rect x="11" y="19.5" width="2" height="3" rx="1" fill={color} transform="rotate(45 12 12)" />
+  <rect x="11" y="19.5" width="2" height="3" rx="1" fill={color} transform="rotate(-45 12 12)" />
 </BaseIcon>
